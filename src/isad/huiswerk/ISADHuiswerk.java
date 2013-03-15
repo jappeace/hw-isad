7c8fe3fe9449ca1d1154c2b5fe01047f3b57bf1d
jappeace <jappieklooster@hotmail.com>
2013-03-15 19:55:43 +0100

    generated code forgot @overide, :O

--- src/isad/huiswerk/ISADHuiswerk.java
+++ src/isad/huiswerk/ISADHuiswerk.java
@@ -41,6 +41,7 @@ public class ISADHuiswerk {
 
 		/* Create and display the form */
 		java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				new GUI().setVisible(true);
 			}
