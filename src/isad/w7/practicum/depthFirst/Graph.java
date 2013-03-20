8060fbaee46162d5d9fdcf6a663e2db4f7525108
jappeace <jappieklooster@hotmail.com>
2013-03-20 18:37:00 +0100

    added a short circuit function

--- src/isad/w7/practicum/depthFirst/Graph.java
+++ src/isad/w7/practicum/depthFirst/Graph.java
@@ -4,7 +4,12 @@
  */
 package isad.w7.practicum.depthFirst;
 
+import isad.w6.graph.Edge;
+import isad.w6.graph.Path.PathStrategy;
+import isad.w6.graph.Path.Vertex.Status;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,5 +24,22 @@ public class Graph<T> extends isad.w6.graph.Path.Graph<T> {
 	@Override
 	public void add(T data){
 		getVertexes().put(data, new Vertex<T>(data));
+	}
+	
+	public isad.w6.graph.Path.Graph<T> shortCircut(){
+		
+		List<isad.w6.graph.Path.Vertex<T>> list = this.listVertexes();
+		this.clear();
+		isad.w6.graph.Path.Vertex<T> last = list.get(list.size()-1);
+		this.setStrategy(PathStrategy.Strategy.NegativeWeighted);
+		
+		return this.Find(last.getData());
+	}
+	
+	private void add(Vertex<T> vertex){
+		this.getVertexes().put(vertex.getData(), vertex);
+	}
+	private void add(isad.w6.graph.Path.Vertex<T> vertex){
+		add((Vertex<T>) vertex);
 	}
 }
